b435aae9e1cad8f58c4e27f8a43b482b142b2262
quangchien0806 <quangchien220399@gmail.com>
2021-01-09 16:07:45 +0700

    Get User Info

---
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/controllers/UserController.java
@@ -0,0 +1,19 @@
+package com.bezkoder.spring.jwt.mongodb.controllers;
+
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/public/user")
+public class UserController {
+    @GetMapping("/me")
+	public ResponseEntity<?> infoUser(Principal principal){
+		return ResponseEntity.ok(principal);
+	}
+}
